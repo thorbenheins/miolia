9fdadc5cc16dacce3b0db664b9dcafbef0e5fea2
Nils Langner <nils.langner@phmLabs.com>
2014-11-22 16:42:10 +0100

    Fixed some missing commas

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,7 +17,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Amilio\CoreBundle\AmilioCoreBundle(),
-            new FOS\UserBundle\FOSUserBundle()
+            new FOS\UserBundle\FOSUserBundle(),
             new Amilio\ShopBundle\AmilioShopBundle(),
         );
 
