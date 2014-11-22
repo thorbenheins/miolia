dcec83c06147f820b8fd858e3f9807b70e2a99cb
Thorben Heins <mail@thorben-heins.de>
2014-11-22 16:46:47 +0100

    tests

--- app/test.php
+++ app/test.php
@@ -1,3 +1,3 @@
 <?php 
 echo
-base64_encode("http://www.amazon.de/gp/product/B004FUF7GS/ref=s9_psimh_gw_p21_d14_i4?pf_rd_m=A3JWKAKR8XB7XF&pf_rd_s=center-2&pf_rd_r=1FY5Q7HQD0F9K1C5AMSH&pf_rd_t=101&pf_rd_p=455353687&pf_rd_i=301128");
+base64_encode("http://www.amazon.de/Erfolgreiche-Softwareprojekte-Web-Gedanken-Webentwicklung-ebook/dp/B00BIUFGRI/ref=sr_1_10?ie=UTF8&qid=1416670722&sr=8-10&keywords=langner") . "\n";
