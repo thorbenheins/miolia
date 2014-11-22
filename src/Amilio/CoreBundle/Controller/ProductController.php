9932d3d02a75e0865de52bbdf7598b391d7c3fb2
Nils Langner <nils.langner@phmlabs.com>
2014-11-22 16:03:03 -0500

    Added fosUserBundle

--- src/Amilio/CoreBundle/Controller/ProductController.php
+++ src/Amilio/CoreBundle/Controller/ProductController.php
@@ -2,12 +2,15 @@
 
 namespace Amilio\CoreBundle\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
+
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function storeAction(Request $request) 
     {
-        return $this->render('AmilioCoreBundle:Default:index.html.twig', array('name' => $name));
+        var_dump( $request->get('url'));
+        return $this->render('AmilioCoreBundle:Default:index.html.twig', array('name' => ''));
     }
 }
