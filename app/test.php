bca794b8e637e61c08ee21586adac36abcae40f7
Thorben Heins <mail@thorben-heins.de>
2014-11-22 17:55:46 +0100

    urlencoded test

--- app/test.php
+++ app/test.php
@@ -1,3 +1,3 @@
 <?php 
 echo
-base64_encode("http://www.amazon.de/Erfolgreiche-Softwareprojekte-Web-Gedanken-Webentwicklung-ebook/dp/B00BIUFGRI/ref=sr_1_10?ie=UTF8&qid=1416670722&sr=8-10&keywords=langner") . "\n";
+urlencode(base64_encode("http://www.amazon.de/Erfolgreiche-Softwareprojekte-Web-Gedanken-Webentwicklung-ebook/dp/B00BIUFGRI/ref=sr_1_10?ie=UTF8&qid=1416670722&sr=8-10&keywords=langner")) . "\n";
