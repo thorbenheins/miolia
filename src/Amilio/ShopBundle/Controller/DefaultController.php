b4506e2a7edd03db77a8af094e161d39951a5660
Thorben Heins <mail@thorben-heins.de>
2014-11-22 17:56:36 +0100

    Moved code from controller to retriever

--- src/Amilio/ShopBundle/Controller/DefaultController.php
+++ src/Amilio/ShopBundle/Controller/DefaultController.php
@@ -4,24 +4,21 @@ namespace Amilio\ShopBundle\Controller;
 
 use Amilio\ShopBundle\ProductRetriever\AmazonRetriever;
 use Amilio\ShopBundle\ProductRetriever\ProductRetriever;
+use ApaiIO\ApaiIO;
+use ApaiIO\Operations\Lookup;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
     public function getProductInfoAction($productUrl)
     {
-
         $url = base64_decode($productUrl);
 
-        //check if it is amazon
-
         /** @var ProductRetriever $retriever */
-        $retriever = new AmazonRetriever();
-
+        $retriever = new AmazonRetriever($this->get("apaiio"));
 
         //get the info from the site
-
-        //
+        $retriever->retrieve($url);
 
         return $this->render('AmilioShopBundle:Default:index.html.twig', array('name' => $url));
     }
