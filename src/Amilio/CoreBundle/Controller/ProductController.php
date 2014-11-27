01fd9b8d0771535a5a49752c7cd77b1a44693ab7
Nils Langner <nils.langner@phmlabs.com>
2014-11-27 09:24:30 -0500

    Added product:show with linked channels

--- src/Amilio/CoreBundle/Controller/ProductController.php
+++ src/Amilio/CoreBundle/Controller/ProductController.php
@@ -9,52 +9,50 @@ use Symfony\Component\HttpFoundation\Request;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class ProductController extends Controller {
-	public function newAction($channelId) {
-	    // @todo use $channel as parameter and let sf2 do the rest
-		$form = $this
-				->createForm(new ProductType(), new Product(),
-						array(
-								'action' => $this
-										->generateUrl(
-												'amilio_core_product_store',
-												array("channelId" => $channelId))));
+class ProductController extends Controller
+{
+    public function newAction($channelId)
+    {
+        // @todo use $channel as parameter and let sf2 do the rest
+        $form = $this
+                ->createForm(new ProductType(), new Product(), array('action' => $this->generateUrl('amilio_core_product_store', array("channelId" => $channelId))));
 
-		return $this
-				->render('AmilioCoreBundle:Product:new.html.twig',
-						array('form' => $form->createView()));
-	}
+        return $this->render('AmilioCoreBundle:Product:new.html.twig', array('form' => $form->createView()));
+    }
 
-	public function storeAction(Request $request, $channelId) {
-		$em = $this->getDoctrine()->getManager();
-		$form = $this
-				->createForm(new ProductType(), new Product(),
-						array(
-								'action' => $this
-										->generateUrl(
-												'amilio_core_product_store', array("channelId" => $channelId))));
+    public function storeAction(Request $request, $channelId)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $form = $this
+                ->createForm(new ProductType(), new Product(), array('action' => $this->generateUrl('amilio_core_product_store', array("channelId" => $channelId))));
 
-		$form->handleRequest($request);
+        $form->handleRequest($request);
 
-		if ($form->isValid()) {
+        if ($form->isValid()) {
 
-			$channel = $this->getDoctrine()->getEntityManager()
-					->find('AmilioCoreBundle:Channel', $channelId);
+            $channel = $this->getDoctrine()->getEntityManager()->find('AmilioCoreBundle:Channel', $channelId);
 
-			$product = $form->getData();
+            $product = $form->getData();
 
-			$product->addChannel($channel);
+            $product->addChannel($channel);
 
-			$em->persist($product);
+            $em->persist($product);
 
-			$em->flush();
+            $em->flush();
 
-			return $this
-					->redirect($this->generateUrl('amilio_core_channel_show', array( "channelName" => $channel->getCanonicalName(), "userName" => $channel->getOwner()->getUsername())));
-		}
+            return $this
+                    ->redirect(
+                            $this
+                                    ->generateUrl('amilio_core_channel_show',
+                                            array("channelName" => $channel->getCanonicalName(), "userName" => $channel->getOwner()->getUsername())));
+        }
 
-		return $this
-				->render('AmilioCoreBundle:Product:new.html.twig',
-						array('form' => $form->createView()));
-	}
+        return $this->render('AmilioCoreBundle:Product:new.html.twig', array('form' => $form->createView()));
+    }
+
+    public function showAction($productId)
+    {
+        $product = $this->getDoctrine()->getEntityManager()->find('AmilioCoreBundle:Product', $productId);
+        return $this->render('AmilioCoreBundle:Product:show.html.twig', array('product' => $product, 'channels' => $product->getChannels()));
+    }
 }
