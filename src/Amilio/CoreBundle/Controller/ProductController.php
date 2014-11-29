b9dc228647ab4474ad47c4be1b6e9c4b98494fea
Nils Langner <nils.langner@phmlabs.com>
2014-11-29 01:28:39 -0500

    Added missing file

--- src/Amilio/CoreBundle/Controller/ProductController.php
+++ src/Amilio/CoreBundle/Controller/ProductController.php
@@ -13,9 +13,7 @@ class ProductController extends Controller
 {
     public function newAction($channelId)
     {
-        // @todo use $channel as parameter and let sf2 do the rest
-        $form = $this
-                ->createForm(new ProductType(), new Product(), array('action' => $this->generateUrl('amilio_core_product_store', array("channelId" => $channelId))));
+        $form = $this->createForm(new ProductType(), new Product(), array('action' => $this->generateUrl('amilio_core_product_store', array("channelId" => $channelId))));
 
         return $this->render('AmilioCoreBundle:Product:new.html.twig', array('form' => $form->createView()));
     }
@@ -23,28 +21,21 @@ class ProductController extends Controller
     public function storeAction(Request $request, $channelId)
     {
         $em = $this->getDoctrine()->getManager();
-        $form = $this
-                ->createForm(new ProductType(), new Product(), array('action' => $this->generateUrl('amilio_core_product_store', array("channelId" => $channelId))));
+        $form = $this->createForm(new ProductType(), new Product(), array('action' => $this->generateUrl('amilio_core_product_store', array("channelId" => $channelId))));
 
         $form->handleRequest($request);
 
         if ($form->isValid()) {
 
             $channel = $this->getDoctrine()->getManager()->find('AmilioCoreBundle:Channel', $channelId);
-
             $product = $form->getData();
-
             $product->addChannel($channel);
-
             $em->persist($product);
-
             $em->flush();
 
             return $this
                     ->redirect(
-                            $this
-                                    ->generateUrl('amilio_core_channel_show',
-                                            array("channelName" => $channel->getCanonicalName(), "userName" => $channel->getOwner()->getUsername())));
+                            $this->generateUrl('amilio_core_channel_show', array("channelName" => $channel->getCanonicalName(), "userName" => $channel->getOwner()->getUsername())));
         }
 
         return $this->render('AmilioCoreBundle:Product:new.html.twig', array('form' => $form->createView()));
@@ -52,7 +43,7 @@ class ProductController extends Controller
 
     public function showAction(Product $product, $canonicalName)
     {
-        if ( $product->getCanonicalName() != $canonicalName ) {
+        if ($product->getCanonicalName() != $canonicalName) {
             return $this->redirect($this->generateUrl('amilio_core_product_show', array('product' => $product->getId(), 'canonicalName' => $product->getCanonicalName())), 302);
         }
         return $this->render('AmilioCoreBundle:Product:show.html.twig', array('product' => $product, 'channels' => $product->getChannels()));
