c915012fb1cb7f63ac6ea00fd316930df57fe9a5
Nils Langner <nils.langner@phmlabs.com>
2014-12-12 15:24:52 -0500

    updated channel show to new layout

--- src/Amilio/CoreBundle/Controller/DefaultController.php
+++ src/Amilio/CoreBundle/Controller/DefaultController.php
@@ -9,12 +9,12 @@ class DefaultController extends Controller
     public function indexAction()
     {
         // $newest = $this->getDoctrine()->getRepository('AmilioCoreBundle:Channel')->findBy(array(), array('id' => 'DESC'), 20, 0);
-        $channelsOfTheWeek = $this->getDoctrine()->getRepository('AmilioCoreBundle:Channel')->findChannelsOfTheWeek();
+        // $channelsOfTheWeek = $this->getDoctrine()->getRepository('AmilioCoreBundle:Channel')->findChannelsOfTheWeek();
         
         $newest = $this->getDoctrine()->getRepository('AmilioCoreBundle:ChannelElement')->findBy(array(), array('id' => 'DESC'), 10, 0);
         
         return $this->render('AmilioCoreBundle:Default:index.html.twig', array(
-            'newest' => $newest, 'channelsOfTheWeek' => $channelsOfTheWeek
+            'newest' => $newest
         ));
     }   
 }
