1f9fc25130b8269c031e682d60272188f720b80b
Nils Langner <nils.langner@phmlabs.com>
2014-12-12 15:03:23 -0500

    Added newest products to homepage

--- src/Amilio/CoreBundle/Controller/DefaultController.php
+++ src/Amilio/CoreBundle/Controller/DefaultController.php
@@ -8,8 +8,10 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        $newest = $this->getDoctrine()->getRepository('AmilioCoreBundle:Channel')->findBy(array(), array('id' => 'DESC'), 20, 0);
+        // $newest = $this->getDoctrine()->getRepository('AmilioCoreBundle:Channel')->findBy(array(), array('id' => 'DESC'), 20, 0);
         $channelsOfTheWeek = $this->getDoctrine()->getRepository('AmilioCoreBundle:Channel')->findChannelsOfTheWeek();
+        
+        $newest = $this->getDoctrine()->getRepository('AmilioCoreBundle:ChannelElement')->findBy(array(), array('id' => 'DESC'), 10, 0);
         
         return $this->render('AmilioCoreBundle:Default:index.html.twig', array(
             'newest' => $newest, 'channelsOfTheWeek' => $channelsOfTheWeek
