3095a001095ab604fc713565302a60d36494be5a
Nils Langner <nils.langner@phmlabs.com>
2014-12-27 07:59:46 -0500

    refactoring of modal handling

---
+++ src/Amilio/CoreBundle/Resources/public/js/modal.js
@@ -0,0 +1,47 @@
+var oldUrl;
+var newUrl;
+
+function showModal(url, width) {
+	newUrl = url;
+
+	if (width) {
+		$("#modalDialog").width(width);
+	}
+	$.get(url, function(data) {
+		$.modal.close();
+		oldUrl = window.location.href;
+		$("#modalDialog").modal({
+			opacity : 80,
+			overlayCss : {
+				backgroundColor : "#000"
+			},
+			onClose : function() {
+				window.history.pushState('', '', oldUrl);
+				$.modal.close();
+			}
+		});
+		$("#modalDialogContent").html(data);
+		processUserFields();
+		window.history.pushState('', '', newUrl);
+	});
+}
+
+function showProductModal(id, name) {
+	var url = product_element_show_path;
+	url = url.replace("elementid", id);
+	url = url.replace("canonicalname", name);
+
+	showModal(url, "800px");
+}
+
+var elementToRemove;
+
+function confirmElementRemove(id) {
+	elementToRemove = id;
+	confirm(function() {
+		url = product_element_remove_path.replace("elementid", elementToRemove);
+		$.post(url, function(data) {
+			location.reload();
+		});
+	});
+}
